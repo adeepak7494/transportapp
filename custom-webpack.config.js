495e33b173141307d27cb948b8c10460820dd37a
adeepak7494 <akhildeepak01@gmail.com>
2023-07-24 10:33:42 +0530

    change for code sandbox

---
+++ custom-webpack.config.js
@@ -0,0 +1,7 @@
+const webpack = require('webpack');
+
+module.exports = {
+    devServer: {
+        allowedHosts: ['https://6rf89w-4200.csb.app'] // <  Copy and paste the domain from the URL Codebox provides for your runtime here
+     },
+};
